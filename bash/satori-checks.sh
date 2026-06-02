ca40eba081764729845ba7152c7c126fe105fdba
Adam <ageodru@gmail.com>
2026-06-02 17:02:57 -0400

    removing old cluster check script

--- bash/satori-checks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# launch sshuttle
-sshuttle -r root@satori-route.mit.edu 192.168.0.0/16 172.30.0.0/16 172.16.0.0/16 172.29.0.0/16
-
-# run python script
-python3 /home/adam/Documents/Python/satori-checks.py
